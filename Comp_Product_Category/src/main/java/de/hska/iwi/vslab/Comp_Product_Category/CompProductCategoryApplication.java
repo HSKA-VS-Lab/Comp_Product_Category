9e39c6b02b3a24abfaf9991969ca451325457106
and1kl <40032530+and1kl@users.noreply.github.com>
2019-11-12 18:34:52 +0100

    Spring Boot Project Initial

---
+++ Comp_Product_Category/src/main/java/de/hska/iwi/vslab/Comp_Product_Category/CompProductCategoryApplication.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Comp_Product_Category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompProductCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompProductCategoryApplication.class, args);
+	}
+
+}
---
+++ Comp_Product_Category/src/test/java/de/hska/iwi/vslab/Comp_Product_Category/CompProductCategoryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hska.iwi.vslab.Comp_Product_Category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompProductCategoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
