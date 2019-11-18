5c73c147e7bc0ecb5d1d41979775ed7124896c70
BobFlop <BobFlop@users.noreply.github.com>
2019-11-18 23:15:24 +0100

    fixed id

--- Comp_Product_Category/src/main/java/de/hska/iwi/vslab/Comp_Product_Category/Controllers/ProductCategoryController.java
+++ Comp_Product_Category/src/main/java/de/hska/iwi/vslab/Comp_Product_Category/Controllers/ProductCategoryController.java
@@ -18,7 +18,7 @@ public class ProductCategoryController {
     /**
      * Checks if categoryId actually exists, if yes then the product is added. 
      */
-    @PostMapping("/comp_product_category/product/{id}")
+    @PostMapping("/comp_product_category/product")
     public void addProduct(@RequestBody String name, double price, int categoryId, String details) { 
         log.info("addProduct(name, price, categoryId, details) was called");
         productCategoryService.addProduct(name, price, categoryId, details);
@@ -28,9 +28,9 @@ public class ProductCategoryController {
      * Delete a category and all products that were in that category. 
      */
     @DeleteMapping("/comp_product_category/category/{id}")
-    public long deleteCategory(@PathVariable int id){
+    public void deleteCategory(@PathVariable int id){
         log.info("deleteCategory(id) was called");
-        return productCategoryService.deleteCategory(id);
+        productCategoryService.deleteCategory(id);
     }
 
 }
