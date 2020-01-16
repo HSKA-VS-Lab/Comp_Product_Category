ed3ec5351c59a9ad6873dbc71d5869c37504be30
Marls2910 <marleen.eisenacht@gmx.de>
2020-01-16 10:59:04 +0100

    hystrix fallback

--- Comp_Product_Category/src/main/java/de/hska/iwi/vslab/Comp_Product_Category/Controllers/ProductCategoryController.java
+++ Comp_Product_Category/src/main/java/de/hska/iwi/vslab/Comp_Product_Category/Controllers/ProductCategoryController.java
@@ -23,7 +23,7 @@ public class ProductCategoryController {
      * Checks if categoryId actually exists, if yes then the product is added.
      */
     @PostMapping(path = "/comp_product_category/product", consumes = "application/json")
-    @HystrixCommand(fallbackMethod = "defaultFallback")
+    //@HystrixCommand(fallbackMethod = "defaultFallback")
     public void addProduct(@RequestBody(required = true) Product request) {
         log.info("addProduct(name, price, categoryId, details) was called");
         productCategoryService.addProduct(request.getName(), request.getPrice(), request.getCategoryId(),
@@ -34,7 +34,7 @@ public class ProductCategoryController {
      * Delete a category and all products that were in that category.
      */
     @DeleteMapping("/comp_product_category/category/{id}")
-    @HystrixCommand(fallbackMethod = "defaultFallback")
+    //@HystrixCommand(fallbackMethod = "defaultFallback")
     public void deleteCategory(@PathVariable int id) {
         log.info("deleteCategory(id) was called");
         productCategoryService.deleteCategory(id);
